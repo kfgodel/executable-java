f33210d8e01a5c51ab4158fd0391883931a5358b
Dario L Garcia <dario.garcia@kfgodeldev.com>
2015-02-17 15:50:08 -0300

    0.1 release

--- src/main/java/ar/com/kfgodel/executablejava/ExecutableMain.java
+++ src/main/java/ar/com/kfgodel/executablejava/ExecutableMain.java
@@ -16,6 +16,7 @@ public class ExecutableMain {
     public static void main(String[] args) throws IOException {
         LOG.info("Example app - Replace this application jar with your own in app/executable-app.jar");
 
+        // Hook that will tell us when the VM is shutting down, so we can free resources or whatever
         Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
             public void run() {
@@ -25,6 +26,7 @@ public class ExecutableMain {
         
         LOG.info("Started example app");
         LOG.info("Press 'r' to restart the hosting JVM. Anything else to end the app");
+        
         int readCHar = System.in.read();
         if((char)readCHar == 'r'){
             LOG.info("Restart requested");
